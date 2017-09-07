14501a5703f2364d263264aef493a53ceb87f809
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2017-09-07 06:56:27 -0700

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n>0)
+printf("its positive");
+else if(n<0)
+printf("its negative")
+else
+printf("its equal to zero");
+}
