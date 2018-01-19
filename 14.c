86ce25a188d658c33a86ed4eecba987381f6e76f
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-01-19 16:16:07 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+int num,s,t,d; 
+        printf("Enter the Range Between s,t ");
+
+        scanf("%d%d", &s,&t);
+
+       for (num = s; num <= t; num++)
+
+            {
+
+               for(d = 2; d < num; d++)
+
+                {
+
+                   if (num % d ==0)
+
+                }
+
+                   if (d == num)
+
+                      printf("%d ", num);
+
+            }
+
