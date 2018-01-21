ebb2c69c5ae08f5ef8dc514c13db015bde68c108
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-01-21 09:47:10 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -1,53 +1,53 @@
 
 #include<stdlib.h>
 #include<stdio.h>
-void merge(int arr[], int l, int m, int r)
+void merge(int a[], int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 =  r - m;
-    int L[n1], R[n2];
+    int L[n1], s[n2];
     for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
+        L[i] = a[l + i];
     for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
+        s[j] = a[m + 1+ j];
     i = 0; 
     j = 0; 
     k = l; 
     while (i < n1 && j < n2)
     {
-        if (L[i] <= R[j])
+        if (L[i] <= s[j])
         {
-            arr[k] = L[i];
+            a[k] = L[i];
             i++;
         }
         else
         {
-            arr[k] = R[j];
+            a[k] = s[j];
             j++;
         }
         k++;
     }
     while (i < n1)
     {
-        arr[k] = L[i];
+        a[k] = L[i];
         i++;
         k++;
     }
     while (j < n2)
     {
-        arr[k] = R[j];
+        a[k] = s[j];
         j++;
         k++;
     }
 }
-void mergeSort(int arr[], int l, int r)
+void mergeSort(int a[], int l, int r)
 {
     if (l < r)
     {
         int m = l+(r-l)/2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
+        mergeSort(a, l, m);
+        mergeSort(a, m+1, r);
  
         merge(arr, l, m, r);
     }
