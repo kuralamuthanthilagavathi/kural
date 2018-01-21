4e0721caa86b62b7d19e003f7827a9ec1306f366
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-01-21 12:08:21 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char str[100];
+int i,line=0;
+printf("Enter the paragraph:",str[i]);
+scanf("%c",&str[i]);
+for(i=0;str[i]!='\n';i++)
+{
+if(str[i]=\n)
+{
+line+1;
+}
+printf("enter the number of lines:",line);
+}
+}
