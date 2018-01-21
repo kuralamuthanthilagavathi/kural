a67f552efeb7a54d3ce295e19d5c913a06e99524
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-01-21 11:04:37 +0530

    Update 30.c

--- 30.c
+++ 30.c
@@ -9,6 +9,6 @@ printf("enter the minute:");
 scanf("%d%d",&m1,&m2);
 h=h2-h1;
 m=m2-m1;
-printf("Enter the time as hr:min",hr:min);
-scanf("%d",&hr:min);
+printf("Enter the time as hr:min",h:m);
+scanf("%d",&h:m);
 }
