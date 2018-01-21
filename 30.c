2ee94f015088673a10a8cc61d9cb34478d030dbf
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-01-21 11:02:34 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int h1,h2,m1,m2;
+int h,m;
+printf("Enter the hour :");
+scanf("%d%d",&h1,&h2);
+printf("enter the minute:");
+scanf("%d%d",&m1,&m2);
+h=h2-h1;
+m=m2-m1;
+printf("Enter the time as hr:min",hr:min);
+scanf("%d",&hr:min);
+}
