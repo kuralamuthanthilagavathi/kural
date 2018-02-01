cb54e6d2fd16495723e00bd62cb1288767dc6f98
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-02-01 16:08:20 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i,add;
+printf("Enter the fixed number:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+add=sum+1;
+}
+printf("%d is the next number of the %d",add,n);
+return 0;
+}
