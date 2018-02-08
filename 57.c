2f579945a343a1e69a83b0a9cdeaf579d875d68b
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-02-08 08:51:01 +0000

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,j,temp;
+printf("Enter the first number:");
+scanf("%d",&i);
+printf("Enter the second number:");
+scanf("%d",&j);
+temp=i;
+i=j;
+j=temp;
+return 0;
+}
