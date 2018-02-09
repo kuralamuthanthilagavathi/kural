5a07585908b168931e30c4ff5b16021082c1cc89
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-02-09 09:44:45 +0000

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n);
+if(n%7==0)
+printf("It is multiples of 7");
+else
+printf("It is not a multiples of 7");
+return 0;
+}
