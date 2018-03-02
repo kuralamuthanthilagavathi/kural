050bbd22f5ac20def6cee7e620a5a52dc2191520
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-03-02 16:38:43 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a;
+    int b;
+    printf("Enter the number:");
+    scanf("%f",&a);
+    b=ceil(a);
+    printf("%d",b);
+    return 0;
+}
