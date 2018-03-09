aa3bfbd5aab2eb2d0f93f70e97d0dd9a06d1bbe1
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-03-09 16:39:09 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int i,s=1,d,j;
+    int s[20];
+    while(s!=0)
+    {
+        for(i=0;i<2;i++)
+        scanf("%d",&a[i]);
+        if(a[i]!=EOF)
+        {
+            for(i=0;i<2;i++)
+        {
+           for(j=i+1;j<2;j++)
+            {
+                d=a[j]-a[i];
+                printf("%d\n",d);
+            }
+        }
+        }
+    }
+}
