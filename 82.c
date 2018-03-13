d08a3233a2e1b42ae8def694213cb0f714b0fa62
kuralamuthanthilagavathi <30829319+kuralamuthanthilagavathi@users.noreply.github.com>
2018-03-13 15:53:58 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float length,breadth,area;
+printf("Enter the length and breadth of the form:");
+scanf("%f%f",&length,&breadth);
+area=length*breadth;
+printf("Enter the area:%f",area);
+return 0;
+}
